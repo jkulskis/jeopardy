f0efc6b0e805dde240642e5b8c1446da1f06ffdb
John Mikulskis <jkulskis@bu.edu>
2020-06-26 00:56:21 -0400

    starting to get a better framework for multiplayer gameplay

--- public/app.js
+++ public/app.js
@@ -6,16 +6,38 @@ const IO = {
   },
 
   bindEvents: () => {
+    // connection
     IO.socket.on("connected", IO.onConnected);
+    // intro screen
     IO.socket.on("newGameCreated", IO.onNewGameCreated);
     IO.socket.on("gameJoined", IO.onGameJoined);
+    // lobby
     IO.socket.on("updateLobby", IO.onUpdateLobby);
+    // gameplay
+    IO.socket.on("startGame", IO.onStartGame);
+    IO.socket.on("createBoard", IO.onCreateBoard);
+    IO.socket.on("updateScoreBoard", IO.onUpdateScoreBoard);
+    IO.socket.on("showClue", IO.onShowClue);
+    IO.socket.on("showClueAnswer", IO.onShowClueAnswer);
+    IO.socket.on("playerHasTurn", IO.onTurn);
+    IO.socket.on("playerTimerCountdown", IO.onPlayerTimerCountdown);
+    IO.socket.on("inquireOtherAnswers", (data) => {
+      App.inquireOtherAnswers(data);
+    });
+    // other
+    IO.socket.on("makeOwner", IO.onMakeOwner);
+    IO.socket.on("showStatus", IO.onShowStatus);
+    // debug/errors
     IO.socket.on("debug", IO.debug);
     IO.socket.on("showError", IO.onError);
   },
 
   onConnected: (data) => {
     console.log(data.message);
+  },
+
+  onMakeOwner: () => {
+    App.Player.isOwner = true;
   },
 
   debug: (data) => {
@@ -31,6 +53,7 @@ const IO = {
 
   onNewGameCreated: (gameID) => {
     App.Player.isOwner = true;
+    App.Player.hasTurn = true;
     App.Game.init(gameID, true);
     App.showLobbyScreen(gameID);
   },
@@ -39,8 +62,8 @@ const IO = {
    * @param data {gameID: *, playerNames: *, error: *}
    */
   onGameJoined: (data) => {
-      App.showLobbyScreen(data.gameID, data.playerNames);
-      App.Game.init(data.gameID, false);
+    App.showLobbyScreen(data.gameID, data.playerNames);
+    App.Game.init(data.gameID, false);
   },
   /**
    * @param {names: *, reset: bool} data
@@ -48,6 +71,48 @@ const IO = {
   onUpdateLobby: (data) => {
     if (data.reset) App.resetPlayerNames();
     data.names.forEach((name) => App.addPlayerName(name));
+    App.lobbyOwnerToggle();
+  },
+
+  onStartGame: (data) => {
+    App.startGame();
+  },
+
+  /**
+   *
+   * @param {board: board to create, round: round we are on} data
+   */
+  onCreateBoard: (data) => {
+    // reset having a trun, emit to the one who has a turn and then
+    // remove the nohover class
+    App.Player.hasTurn = false;
+    App.createBoard(data.board, data.round);
+    $(".cell.clue").addClass("nohover");
+  },
+
+  onTurn: (data) => {
+    App.Player.hasTurn = true;
+    $(".cell.clue").removeClass("nohover");
+  },
+
+  onUpdateScoreBoard: (data) => {
+    App.updateScoreBoard(data.players, data.reset);
+  },
+
+  onShowClue: (data) => {
+    App.showClue(data.question);
+  },
+
+  onShowClueAnswer: (data) => {
+    App.showClueAnswer(data.answer);
+  },
+
+  onShowStatus: (data) => {
+    App.showStatus(data.message);
+  },
+
+  onPlayerTimerCountdown: (data) => {
+    App.playerTimerCountdown(data.playerID);
   },
 };
 
@@ -68,19 +133,21 @@ const App = {
     // Templates
     App.$gameArea = $("#gameArea");
     App.$templateIntroScreen = $("#intro-screen-template").html();
-    // App.$templateNewGame = $('#create-game-template').html();
-    // App.$templateJoinGame = $('#join-game-template').html();
     App.$templateLobby = $("#lobby-template").html();
+    App.$templateGamePlay = $("#gamePlay-template").html();
   },
 
   showError: (error) => {
     if ($("#errorMessage").length) {
-      $("#errorMessage").stop().show().animate({opacity:'100'}).text(error);
+      $("#errorMessage").stop().show().animate({ opacity: "100" }).text(error);
       setTimeout(() => $("#errorMessage").fadeOut(3000), 500);
-    }
-    else {
+    } else {
       alert(error);
     }
+  },
+
+  showStatus: (message) => {
+    $("#gameStatus").text(message);
   },
 
   showInitScreen: () => {
@@ -93,14 +160,24 @@ const App = {
     App.$gameArea.html(App.$templateLobby);
     if (playerNames !== undefined) {
       playerNames.forEach((name) => App.addPlayerName(name));
+      App.addPlayerName(App.Player.name);
+    } else {
+      // if no other names, must be creating a game, and must be owner
+      App.addPlayerName(App.Player.name + " (Owner)");
     }
-    // have to add the player's own name as well!
-    App.addPlayerName(App.Player.name);
 
     $("#gameURL").text(window.location.href);
     $("#gameID").text(gameID);
-    console.log("gameID:", gameID);
-    console.log("game URL:", window.location.href);
+    App.lobbyOwnerToggle();
+  },
+
+  lobbyOwnerToggle: () => {
+    // only the owner can start the game
+    if (App.Player.isOwner) {
+      $("#btnStartGame").show();
+    } else {
+      $("#btnStartGame").hide();
+    }
   },
 
   resetPlayerNames: () => {
@@ -112,10 +189,26 @@ const App = {
   },
 
   bindEvents: () => {
+    // init screen
     App.$doc.on("click", "#btnCreateGame", App.onCreateClick);
     App.$doc.on("click", "#btnLeaveGame", App.onLeaveClick);
     App.$doc.on("click", "#btnJoinGame", App.onJoinClick);
+    // lobby screen
+    App.$doc.on("click", "#btnStartGame", App.onStartClick);
     App.$doc.on("change", "#playerName", App.Player.saveName);
+    // gamePlay screen
+    App.$doc.on("click", ".cell.clue", function () {
+      App.onClueClick($(this).attr("id"));
+    });
+  },
+
+  onClueClick: (clueID) => {
+    if (App.Player.hasTurn)
+      IO.socket.emit("clueChosen", { gameID: App.Game.gameID, clueID: clueID });
+  },
+
+  onStartClick: () => {
+    IO.socket.emit("startGame", { gameID: App.Game.gameID });
   },
 
   onCreateClick: () => {
@@ -135,10 +228,153 @@ const App = {
     App.showInitScreen();
   },
 
+  startGame: () => {
+    App.$gameArea.html(App.$templateGamePlay);
+  },
+
+  createBoard: (board, round) => {
+    $("#questionScreen").addClass("hidden");
+    $("#questionBox").empty(); // in case there is lag when we show it again
+    multiplier = round === "round1" ? 1 : 2;
+    let c = 0;
+    $("#gameBoard").empty();
+    for (let category in board) {
+      $("#gameBoard").append(
+        `<div id="c${c}" col="${c}" class="category"></div>`
+      );
+      $(`#c${c}`).append(
+        `<div id="cheader${c}" class="cell header noselect" col="${c}">${category}</div>`
+      );
+      // clue[0] is the ID of this question's clue
+      let r = 0;
+      board[category].forEach((clue) => {
+        $(`#c${c}`).append(
+          `<div id="${
+            clue["id"]
+          }" class="cell clue noselect" col="${c}" row="${r}">$${
+            clue["value"] * multiplier
+          }</div>`
+        );
+        if (clue["answered"] === 1) {
+          clueCell = $(`#${clue["id"]}`);
+          clueCell.addClass("answered");
+          clueCell.empty();
+        }
+        r += 1;
+      });
+      c += 1;
+    }
+    $(".cell.header").fitText(0.8);
+    $(".cell.clue").fitText(0.4);
+  },
+
+  updateScoreBoard: (players, reset) => {
+    if (reset) {
+      players.forEach((player) => {
+        console.log(JSON.stringify(player));
+        $("#scoreBoard").append(
+          `<div id="${player.id}" class="score-card"></div>`
+        );
+        $(`#${player.id}`).append(
+          `<div class="name-label">${player.name}</div>`
+        );
+        $(`#${player.id}`).append(
+          `<div class="score-label">${player.score}</div>`
+        );
+        $(`#${player.id}`).append(
+          `<div id="timer-${player.id}" class="timer"></div>`
+        );
+        for (let i = 0; i < 9; i++)
+          $(`#timer-${player.id}`).append(
+            `<span class="timerBlock">&nbsp;</span>`
+          );
+      });
+    }
+    $(".name-label").fitText(1.5, {
+      minFontSize: "12px",
+      maxFontSize: "22px",
+    });
+    $(".score-label").fitText(1.5, {
+      minFontSize: "12px",
+      maxFontSize: "22px",
+    });
+  },
+
+  showClue: (question) => {
+    // console.log(question);
+    $("#questionBox").empty(); // remove anything left over here
+    $("#questionScreen").removeClass("hidden");
+    $("#questionBox").append(
+      `<span id="questionText" class="clue-card-span">${question}</span>`
+    );
+    $("#questionBox").append(
+      `<div id="clueDivider" class="divider hidden"></div>`
+    );
+    $("#questionBox").append(
+      `<span id="answerText" class="clue-card-span">&nbsp;</span>`
+    );
+    $("#questionText").fitText(1, { maxFontSize: "95px" });
+    App.Game.setScreen("qWaiting");
+    $(document).on("keyup", (e) => {
+      //console.log(e.keyCode);
+      if (e.keyCode === 32) {
+        App.pressAnswerBuzzer();
+      }
+    });
+  },
+
+  pressAnswerBuzzer: () => {
+    if (App.Game.screen === "qWaiting" && !App.Player.buzzedIn) {
+      // App.getClueAnswer();
+      IO.socket.emit("buzzIn", { gameID: App.Game.gameID });
+      // even if they're not the first to buzz in, it will still be answering
+      // if someone beat them to it
+      App.Game.setScreen("qAnswering");
+      // $(document).off("keyup");
+      // App.clueComplete();
+    }
+  },
+
+  playerTimerCountdown: (playerID) => {
+    App.Game.buzzedPlayerID = playerID;
+    console.log("5s countdown starts now");
+    // start to make timer go
+  },
+
+  inquireOtherAnswers: (data) => {
+    // for now, just show a status. May change this later
+    App.Game.setScreen("qWaiting");
+    App.Game.buzzedPlayerID = null;
+    countdown = (s) => {
+      setTimeout((s) => {
+        App.showStatus(`${s} seconds to buzz in a different answer`);
+        if (s) countdown(s - 1);
+      }, 1000);
+    };
+    countdown(3);
+  },
+
+  getClueAnswer: () => {
+    if ($("answerText").children().length === 0)
+      IO.socket.emit("getClueAnswer", { gameID: App.Game.gameID });
+  },
+
+  showClueAnswer: (answer) => {
+    $("#clueDivider").removeClass("hidden");
+    $("#answerText").text(answer);
+    $("#answerText").fitText(1, { maxFontSize: "95px" });
+  },
+
+  clueComplete: () => {
+    IO.socket.emit("clueComplete", { gameID: App.Game.gameID });
+  },
+
   Player: {
     // player that starts the game
-    // TODO: random player that is still in the game after previous owner leaves
+    // TODO: random player that is still in the game after previoujs owner leaves
     isOwner: false,
+    hasTurn: false,
+    buzzedIn: false,
     // socket.io socket object id. Unique for each player.
     // Generated by the browser when the player initially connects to the server
     sockID: "",
@@ -152,6 +388,8 @@ const App = {
   Game: {
     // identical to ID of the socket.io room of the game
     gameID: null,
+    screen: null,
+    buzzedPlayerID: null,
 
     init: (gameID, isOwner) => {
       App.Game.gameID = gameID;
@@ -160,7 +398,12 @@ const App = {
 
     reset: () => {
       App.Game.gameID = null;
+      App.Game.screen = null;
       App.Player.isOwner = false;
+    },
+
+    setScreen: (screen) => {
+      App.Game.screen = screen;
     },
   },
 };
--- server/game.js
+++ server/game.js
@@ -1,34 +1,135 @@
+const crypto = require("crypto");
+const questions = require(`${__dirname}/../data/processed_jeopardy_questions.json`);
+let categories = Object.keys(questions);
 let io;
 let players = {};
 let games = {};
 
-const Player = (socket) => ({
-  id: socket.id,
-  name: null,
-  isOwner: false,
-  gameID: null, // null if not in a game, otherwise the gameID
-  score: 0,
-  disconnected: false,
-});
+// creates a timeout handler that can prematurely clear/trigger the
+// function connected to the timeout
+// inspired by https://stackoverflow.com/a/36790119/10231083
+createTimeout = (f, delay) => {
+  let timeoutFunction = setTimeout(f, delay);
+  return {
+    clear: () => {
+      clearTimeout(timeoutFunction);
+    },
+    trigger: () => {
+      clearTimeout(timeoutFunction);
+      timeoutFunction();
+    },
+  };
+}
 
-const Game = (socket, gameID) => ({
-  id: gameID,
-  ownerID: socket.id,
-  players: {},
-  status: "lobby",
-});
+function Player(socketID) {
+  this.id = socketID;
+  this.name = null;
+  this.isOwner = false;
+  this.gameID = null; // null if not in a game, otherwise the game's ID
+  this.score = 0;
+  this.disconnected = false;
+}
+
+function Game(socket, gameID) {
+  this.id = gameID;
+  this.ownerID = socket.id;
+  // id of the player who's turn it is (they get to choose the next clue)
+  this.turnPlayerID = socket.id;
+  // id of the player that is currently buzzed in
+  this.buzzedPlayerID = null;
+  // ids of ther players that have answered the last question
+  this.buzzedPlayerIDs = new Map();
+  this.currentClueID = null;
+  this.currentRound = "round1";
+  this.players = {};
+  this.numPlayers = Object.keys(this.players).length;
+  this.status = "lobby";
+  // r1 & r2 have categories as keys, and these categories have question values as keys
+  // which lead to lists of questions...final just has a final jeopardy question
+  // the client board only has category names and ids of questions as keys
+  // with 0 as unanswered and 1 as answered
+  this.clientBoard = { round1: {}, round2: {}, final: {} };
+  // this is a map to have O(1) ability to check if a clue ID is valid,
+  // and if so then grab that question's attributes
+  this.clueMap = new Map();
+
+  this.fillBoards = () => {
+    chosenCategories = new Map();
+    category = categories[(categories.length * Math.random()) << 0];
+    for (let i = 0; i < 12; i++) {
+      // make sure we didn't already choose this category + isn't final jeopardy
+      while (chosenCategories.has(category) || category === "final") {
+        category = categories[(categories.length * Math.random()) << 0];
+      }
+      chosenCategories.set(category, 0);
+      // add questions from this category to the board
+      clientRound = i < 6 ? this.clientBoard.round1 : this.clientBoard.round2;
+      clientRound[category] = [];
+      for (let value in questions[category]) {
+        // list that are at this value...we will choose a random one from it
+        clueList = questions[category][value];
+        randomValueClue = clueList[(clueList.length * Math.random()) << 0];
+        randomValueClue["answered"] = 0;
+        clientClue = {
+          id: randomValueClue["id"],
+          value: randomValueClue["value"],
+          answered: 0,
+        };
+        clientRound[category].push(clientClue);
+        randomValueClue["clientClue"] = clientClue;
+        this.clueMap.set(randomValueClue["id"], randomValueClue);
+      }
+    }
+    randomFinal =
+      questions["final"][(questions["final"].length * Math.random()) << 0];
+    this.clientBoard.final = randomFinal["id"];
+    this.clueMap.set(randomFinal["id"], randomFinal);
+  };
+
+  this.getCurrentAnswer = () => {
+    clue = this.clueMap.get(this.currentClueID);
+    clue.answered = 1;
+    return clue.answer;
+  };
+
+  // every time a player buzzes in, this timeout
+  // function is set to run
+  this.buzzedPlayerTimeout = () => {
+    buzzedPlayerName = this.players[this.buzzedPlayerID].name;
+    io.to(gameID).emit("showStatus", {
+      message: `Time is up for ${buzzedPlayerName}!`,
+    });
+    this.buzzedPlayerID = null;
+    if (this.numPlayers === this.buzzedPlayerIDs.size) {
+      io.to(this.id).emit("scoreQuestion", { players: this.buzzedPlayerIDs });
+    } else {
+      inquireOtherAnswers({ gameID: this.id });
+    }
+  };
+  this.buzzedPlayerTimeoutHandler = null;
+
+  this.waitingForAnswersTimeout = () => {
+    if (!this.buzzedPlayerIDs.size) {
+      sendClueAnswer({gameID: this.id});
+    } else {
+      sendClueAnswer({gameID: this.id});
+      io.to(this.id).emit("scoreQuestion", { players: this.buzzedPlayerIDs });
+    }
+  };
+  this.waitingForAnswersTimeoutHandler = null;
+}
 
 exports.playerConnected = (sio, socket) => {
   io = sio;
   if (socket.id in players) {
     players[socket.id].disconnected = false;
   } else {
-    players[socket.id] = Player(socket.id);
+    players[socket.id] = new Player(socket.id);
   }
   socket.emit("connected", {
     message: `Connected, id is: ${socket.id}`,
   });
-  initGame(socket);
+  initSession(socket);
   socket.emit("debug", players);
   socket.emit("debug", `# of Players: ${Object.keys(players).length}`);
   socket.emit("debug", games);
@@ -67,30 +168,249 @@ exports.playerDisconnected = (socket) => {
   }, 1000);
 };
 
-initGame = (socket) => {
+defined = (...objs) => {
+  objs.forEach((obj) => {
+    if (obj === undefined) {
+      console.log(`Object ${obj} is not defined`);
+      return false;
+    }
+  });
+  return true;
+};
+
+// check if an object has certain keys
+hasKeys = (obj, ...keys) => {
+  for (key of keys) {
+    if (!(key in obj)) {
+      console.log(`Object ${JSON.stringify(obj)} does not have key ${key}`);
+      return false;
+    }
+  }
+  return true;
+};
+
+nameCheck = (name, socket) => {
+  if (!(name && /\S/.test(name))) {
+    socket.emit("showError", { error: "Enter a Name" });
+    return false;
+  }
+  if (!/^[\w\-\s]+$/.test(name)) {
+    socket.emit("showError", {
+      error: "Name contains invalid characters",
+    });
+    return false;
+  }
+  if (name.length > 11) {
+    socket.emit("showError", {
+      error: "Name must be less than 12 characters long",
+    });
+    return false;
+  }
+  return true;
+};
+
+gameExists = (gameID) => {
+  return gameID in games;
+};
+
+errorDataGamePlayer = (data, pSock) => {
+  // make sure the data parameter is fulfilled
+  if (!defined(data)) return 1;
+  // make sure there is a gameID key
+  if (!hasKeys(data, "gameID")) return 1;
+  // make sure the gameID is valid
+  if (!gameExists(data.gameID)) return 1;
+  // make sure the player is in the game
+  if (!(pSock.id in games[data.gameID].players)) return 1;
+  // if we made it this far past all the checks, return 0
+  return 0;
+};
+
+initSession = (socket) => {
   socket.on("createGame", createGame);
   socket.on("joinGame", joinGame);
   socket.on("leaveGame", leaveGame);
+  socket.on("startGame", startGame);
+  socket.on("clueChosen", clueChosen);
+  socket.on("buzzIn", buzzIn);
+  socket.on("getClueAnswer", sendClueAnswer);
+  socket.on("clueComplete", afterClueCompleted);
 };
+
+function startGame(data) {
+  pSock = this;
+  if (errorDataGamePlayer(data, pSock)) return 1;
+  // make sure that the player is in the game and that they are an owner
+  if (players[pSock.id].isOwner === false) return 1;
+  // player must be owner of this particular game to start it
+  if (games[data.gameID].ownerID !== pSock.id) return 1;
+
+  io.to(data.gameID).emit("startGame");
+  games[data.gameID].fillBoards();
+  games[data.gameID].currentRound = "round1";
+  emitBoard({ pSock: pSock, gameID: data.gameID, round: "round1" });
+  updateScoreboard({ gameID: data.gameID, gameStarted: true });
+}
+
+/**
+ *
+ * @param {pSock: socket of player, gameID: id of game, round: *, solo: emit board to pSock only} data
+ */
+function emitBoard(data) {
+  game = games[data.gameID];
+  clientBoard = game.clientBoard;
+  if (!data.solo) {
+    io.to(data.gameID).emit("createBoard", {
+      board: clientBoard[game.currentRound],
+      round: game.currentRound,
+    });
+    io.to(data.gameID).emit("debug", clientBoard);
+  } else {
+    pSock.emit("createBoard", {
+      board: clientBoard[data.currentRound],
+      round: game.currentRound,
+    });
+  }
+  io.to(game.turnPlayerID).emit("playerHasTurn");
+  turnPlayerName = game.players[game.turnPlayerID].name;
+  if (game.numPlayers > 1)
+    io.to(data.gameID).emit("showStatus", {
+      message: `${turnPlayerName}'s turn!`,
+    });
+  // data.pSock.emit("debug", {
+  //   board: clientBoard[data.round],
+  //   round: data.round,
+  // });
+}
+
+/**
+ *
+ * @param {gameID: *, gameStarted: true/false, playerIDs: IDs of players to update} data
+ */
+function updateScoreboard(data) {
+  playerScores = [];
+  if (data.gameStarted) {
+    for (let playerID in games[data.gameID].players) {
+      player = games[data.gameID].players[playerID];
+      playerScores.push({
+        id: player.id,
+        name: player.name,
+        score: player.score,
+      });
+    }
+    return io
+      .to(data.gameID)
+      .emit("updateScoreBoard", { reset: true, players: playerScores });
+  }
+  data.playerIDs.forEach((playerID) => {
+    player = games[data.gameID].players[playerID];
+    playerScores.push({
+      id: player.id,
+      name: player.name,
+      score: player.score,
+    });
+    return io
+      .to(data.gameID)
+      .emit("updateScoreBoard", { players: playerScores });
+  });
+}
+
+/**
+ *
+ * @param {clueID: ID of the chosen clue, gameID: id of game} data
+ */
+function clueChosen(data) {
+  if (errorDataGamePlayer(data, this)) return 1;
+  clueID = data.clueID;
+  game = games[data.gameID];
+  if (game.turnPlayerID !== this.id) return 1;
+  if (!hasKeys(data, "clueID")) return 1;
+  if (game.clueMap.has(clueID) === false) return 1;
+  // DEBUG
+  io.to(data.gameID).emit("debug", "10s waiting for answers timeout");
+  game.waitingForAnswersTimeoutHandler = createTimeout(
+    game.waitingForAnswersTimeout,
+    10000
+  );
+  game.currentClueID = clueID;
+  clue = game.clueMap.get(clueID);
+  clue.clientClue.answered = 1;
+  // sending as object just in case we want to send other info like show #, date, etc.
+  io.to(data.gameID).emit("showClue", {
+    question: clue.question,
+  });
+}
+
+function buzzIn(data) {
+  if (errorDataGamePlayer(data, this)) return 1;
+  game = games[data.gameID];
+  if (game.buzzedPlayerID !== null) return 1;
+  if (game.buzzedPlayerIDs.has(this.id)) return 1; // can't buzz in twice
+  game.buzzedPlayerID = this.id;
+  buzzedPlayerName = game.players[game.buzzedPlayerID].name;
+  io.to(data.gameID).emit("playerTimerCountdown", {
+    playerID: game.buzzedPlayerID,
+  });
+  io.to(data.gameID).emit("showStatus", {
+    message: `${buzzedPlayerName} just buzzed in!`,
+  });
+  // stop the timeout for all answers (timeout if no one buzzes in)
+  game.waitingForAnswersTimeoutHandler.clear();
+  game.buzzedPlayerIDs.set(game.buzzedPlayerID, buzzedPlayerName);
+  // start the player's personal timer to answer the question
+  game.buzzedPlayerTimeoutHandler = createTimeout(game.buzzedPlayerTimeout, 5000);
+}
+
+function inquireOtherAnswers(data) {
+  game = games[data.gameID];
+  io.to(data.gameID).emit("inquireOtherAnswers");
+  io.to(data.gameID).emit("debug", "3s waiting for answers timeout");
+  game.waitingForAnswersTimeoutHandler = createTimeout(
+    game.waitingForAnswersTimeout,
+    3000
+  );
+}
+
+function sendClueAnswer(data) {
+  game = games[data.gameID];
+  if (game.currentClueID === null) return 1;
+  io.to(data.gameID).emit("showClueAnswer", {
+    answer: game.getCurrentAnswer(),
+  });
+  game.currentClueID = null;
+}
+
+function afterClueCompleted(data) {
+  if (errorDataGamePlayer(data, this)) return 1;
+  game = games[data.gameID];
+  if (game.currentClueID !== null) return 1;
+  emitBoard({
+    pSock: this,
+    gameID: data.gameID,
+    round: game.currentRound,
+  });
+}
 
 /**
  *
  * @param {name: playerName, gameID: id of game} data
  */
 function joinGame(data) {
-  if (!(data.name && /\S/.test(data.name))) {
-    return this.emit("showError", {error: `Choose a Better Name!`})
-  }
+  if (!defined(data)) return 1;
+  if (!hasKeys(data, "name")) return 1;
+  if (!nameCheck(data.name, this)) return 1;
   players[this.id].name = data.name;
   result = { gameID: data.gameID, error: null };
   result["playerNames"] = [];
   if (data.gameID in games) {
     for (let playerID in games[data.gameID].players) {
-      result["playerNames"].push(players[playerID].name);
       if (data.name == players[playerID].name) {
         result.error = "Someone in this game already has that name";
         return this.emit("showError", result);
       }
+      if (players[playerID].isOwner)
+        result["playerNames"].push(players[playerID].name + " (Owner)");
+      else result["playerNames"].push(players[playerID].name);
     }
   } else {
     result.error = "This game ID does not exist";
@@ -112,10 +432,6 @@ function joinGame(data) {
  * @param {gameID: *} data
  */
 function leaveGame(data) {
-  if (!("gameID" in data) || !(data.gameID in games)) {
-    console.log("Invalid leave game");
-    return;
-  }
   this.leave(data.gameID);
   players[this.id].gameID = null;
   changeOwner = false;
@@ -142,9 +458,13 @@ resetLobby = (gameID, changeOwner) => {
       // update the first available player to be the new owner
       players[playerID].isOwner = true;
       games[gameID].ownerID = playerID;
+      // let the client know that they are the new owner
+      io.to(playerID).emit("makeOwner");
       changeOwner = false;
     }
-    playerNames.push(players[playerID].name);
+    if (players[playerID].isOwner)
+      playerNames.push(players[playerID].name + " (Owner)");
+    else playerNames.push(players[playerID].name);
   }
   io.to(gameID).emit("updateLobby", { names: playerNames, reset: true });
 };
@@ -153,13 +473,21 @@ resetLobby = (gameID, changeOwner) => {
  * @param {name: playerName} data
  */
 function createGame(data) {
-  if (!(data.name && /\S/.test(data.name))) {
-    return this.emit("showError", {error: `Choose a Better Name!`})
+  if (!defined(data)) return 1;
+  if (!hasKeys(data, "name")) return 1;
+  if (!nameCheck(data.name, this)) return 1;
+  // 6 char string, since each byte is converted to 2 chars
+  let gameID = "";
+  // Just in case there is a collision...
+  while (!gameID || gameID in games) {
+    gameID = crypto.randomBytes(3).toString("hex").toUpperCase();
   }
-  let gameID = (Math.random() * 100000) | 0;
-  games[gameID] = Game(this, gameID);
+  // replace 0 with other characters since it looks the same as 0
+  gameID = gameID.replace(/0/g, "G");
+  games[gameID] = new Game(this, gameID);
   games[gameID].players[this.id] = players[this.id];
   games[gameID].ownerID = this.id;
+  players[this.id].isOwner = true;
   players[this.id].name = data.name;
   players[this.id].gameID = gameID;
   // save the owner socket ID as well as the game ID so that we can
